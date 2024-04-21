79cde6dd9127fa7fb318cfffdaf558f0726d3fa3
karen <karen.fabiana226@gmail.com>
2024-04-21 19:22:16 -0400

    se creó carpeta controllers y se agregó la importación en app.module.ts

--- Backend/uber-eats/src/app.module.ts
+++ Backend/uber-eats/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ControllersModule } from './controllers/controllers.module';
 
 @Module({
-  imports: [],
+  imports: [ControllersModule],
   controllers: [AppController],
   providers: [AppService],
 })
