0d7449b838faaf507f646d9cde60706a51f47533
karen <karen.fabiana226@gmail.com>
2024-04-21 19:55:39 -0400

    se conectó la base de datos en el app.module

--- Backend/uber-eats/src/app.module.ts
+++ Backend/uber-eats/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ControllersModule } from './controllers/controllers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ControllersModule],
+  imports: [ControllersModule,
+    TypeOrmModule.forRoot({
+      type:'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'local_admin',
+      password: 'localadmin',
+      database: 'locales', 
+      entities:[],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
