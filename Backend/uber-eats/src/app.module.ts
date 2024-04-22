e3888c85ccfad53e533015d2c5db87b6ab611747
karen <karen.fabiana226@gmail.com>
2024-04-21 20:10:30 -0400

    se configuró la base de datos provisoria con '.env'

--- Backend/uber-eats/src/app.module.ts
+++ Backend/uber-eats/src/app.module.ts
@@ -2,19 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ControllersModule } from './controllers/controllers.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [ControllersModule,
-    TypeOrmModule.forRoot({
-      type:'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'local_admin',
-      password: 'localadmin',
-      database: 'locales', 
-      entities:[],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get('DB_PORT')),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+      inject:[ConfigService]
     })
   ],
   controllers: [AppController],
