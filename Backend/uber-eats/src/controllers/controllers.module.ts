1fd159dcb54e558c13b20430a006786404d42daf
karen <karen.fabiana226@gmail.com>
2024-04-23 12:39:59 -0400

    se agregó el controller de 'cliente'

--- Backend/uber-eats/src/controllers/controllers.module.ts
+++ Backend/uber-eats/src/controllers/controllers.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { LocalController } from './local/local.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClienteController } from './cliente/cliente.controller';
 import { Local } from  'src/database/entities/local.entity';
 import { LocalService }  from 'src/providers/local/local.service';
 
@@ -8,7 +9,7 @@ import { LocalService }  from 'src/providers/local/local.service';
     imports: [
         TypeOrmModule.forFeature([Local])
     ],
-    controllers:[LocalController],
+    controllers:[LocalController, ClienteController],
     providers:[LocalService]
 })
 export class ControllersModule {}
