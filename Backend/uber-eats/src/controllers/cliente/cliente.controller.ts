9385045faf9d44386e44dff18dc5f652e6fa1297
karen <karen.fabiana226@gmail.com>
2024-04-24 19:40:05 -0400

    se creó el método post de cliente

--- Backend/uber-eats/src/controllers/cliente/cliente.controller.ts
+++ Backend/uber-eats/src/controllers/cliente/cliente.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body} from '@nestjs/common';
+import { IPostClienteRequest } from './dto/IPostClienteRequest';
+import { IPostClienteResponse } from './dto/IPostClienteResponse';
 
 @Controller('cliente')
-export class ClienteController {}
+export class ClienteController {
+    @Post()
+    async postCliente(@Body() request: IPostClienteRequest): Promise <IPostClienteResponse> {
+        console.log('@POST');
+        const response: IPostClienteResponse = {
+            data: null,
+            statusCode:200,
+            statusDescription:"Cliente Agregado",
+            error: null
+
+        };
+        if  (request) {
+
+            const newCliente: ClienteEntity={
+                nombre: request.nombre,
+                apellido: request.apellido,
+                contraseña:  request.contraseña,
+
+            } as ClienteEntity;
+            await this.clienteService.create(newCliente);
+            return response;
+    }
+}
+}
