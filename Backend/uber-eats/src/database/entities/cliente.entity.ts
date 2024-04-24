40e3a130ff77a38a186f1f9ff0145b2d1a1c7e0c
karen <karen.fabiana226@gmail.com>
2024-04-24 19:50:54 -0400

    se creó la entidad cliente

---
+++ Backend/uber-eats/src/database/entities/cliente.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Cliente {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nombre: string;
+
+  @Column()
+  apellido: string;
+
+  @Column()
+  contraseña: number;
+
+}
