a0bc54440b775543b028a0613728f79e15213739
karen <karen.fabiana226@gmail.com>
2024-04-24 20:30:15 -0400

    se agregó la importación de la entidad cliente a 'database.module.ts'

--- Backend/uber-eats/src/database/database.module.ts
+++ Backend/uber-eats/src/database/database.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Local } from 'src/database/entities/local.entity';
+import { Cliente } from 'src/database/entities/cliente.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Local])],
+  imports: [TypeOrmModule.forFeature([Local]), 
+  TypeOrmModule.forFeature([Cliente])],
   exports: [TypeOrmModule],  
 })
 export class DatabaseModule {}
