47e6b0c447aa239c396d33b234bc189e8a3c561b
karen <karen.fabiana226@gmail.com>
2024-04-24 20:16:14 -0400

    se cambió a public el método create

--- Backend/uber-eats/src/providers/cliente/cliente.service.ts
+++ Backend/uber-eats/src/providers/cliente/cliente.service.ts
@@ -8,7 +8,7 @@ export class ClienteService {
     constructor(
         @InjectRepository(Cliente) private readonly clienteRepository: Repository<Cliente>,
     ){}
-    private async create (cliente:Cliente): Promise<Cliente>{
+    public async create (cliente:Cliente): Promise<Cliente>{
         const result= this.clienteRepository.create(cliente)
         return await this.clienteRepository.save(result);
     }
