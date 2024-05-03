e73dde2464c30b21e2e4b69f837cd5974e8a4643
karen <karen.fabiana226@gmail.com>
2024-05-03 15:51:57 -0400

    se agregaron validators

--- Backend/uber-eats/src/controllers/cliente/dto/IPostClienteRequest.ts
+++ Backend/uber-eats/src/controllers/cliente/dto/IPostClienteRequest.ts
@@ -1,6 +1,18 @@
-export interface IPostClienteRequest {
+import {IsNotEmpty, IsNumber, IsString} from "class-validator";
+export class IPostClienteRequest {
+    @IsNotEmpty()
+    @IsNumber()
     id: number;
+
+    @IsNotEmpty()
+    @IsString()
     nombre: string;
+
+    @IsNotEmpty()
+    @IsString()
     apellido: string;
+
+    @IsNotEmpty()
+    @IsString()
     contraseña: string;
 }
--- Backend/uber-eats/src/controllers/cliente/dto/IPostClienteResponse.ts
+++ Backend/uber-eats/src/controllers/cliente/dto/IPostClienteResponse.ts
@@ -1,2 +1,6 @@
 export interface IPostClienteResponse {
+    data : any;
+    statusCode : number;
+    statusDescription : string;
+    error: string;
 }
