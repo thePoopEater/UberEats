3eace26a60f7bd74b730d1530c33a5f058493381
crissspi <cristinatiticortez@gmail.com>
2024-05-06 16:30:11 -0400

    creación clase Local

--- frontEnd/src/app/container/inicio/clases.ts
+++ frontEnd/src/app/container/inicio/clases.ts
@@ -4,7 +4,8 @@ export class Product {
     description : string;
     price: number;
     img: string;
-    cantidad : number
+    cantidad : number;
+
     constructor(id:number, name:string, desc:string, price:number, img: string, cantidad:number){
         this.id = id;
         this.name = name;
@@ -12,6 +13,28 @@ export class Product {
         this.price = price;
         this.img = img;
         this.cantidad = cantidad;
+    }
+}
+
+export class Local {
+    idLocal: number;
+    nombre: string;
+    direccion: string; 
+    tiposProductos: Array<string>;
+    costoEnvio: number;
+    tiempoEntrega: Array<number>;
+    calificacion: number;
+    productos: Array<Product>;
+
+    constructor (idLocal: number, nombre: string, direccion: string, tiposProductos: Array<string>, costoEnvio: number, tiempoEntrega: Array<number>, calificacion: number, productos: Array<Product>){
+        this.idLocal = idLocal;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.tiposProductos = tiposProductos;
+        this.costoEnvio = costoEnvio;
+        this.tiempoEntrega = tiempoEntrega;
+        this.calificacion = calificacion;
+        this.productos = productos;
     }
 }
 
