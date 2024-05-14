76338b2dd8b250612e4c7b1d503c6fcb1425eccc
crissspi <cristinatiticortez@gmail.com>
2024-05-14 16:34:21 -0400

    incorporación carrito service

--- frontEnd/src/app/features/pages/carrito/carrito.component.ts
+++ frontEnd/src/app/features/pages/carrito/carrito.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { CarritoService } from '../../../core/services/carrito-service/carrito.service';
 
 @Component({
   selector: 'app-carrito',
@@ -12,9 +13,9 @@ import { RouterLink } from '@angular/router';
 export class CarritoComponent implements OnInit {
 
   productos = [
-    { nombre: 'Producto 1', precio: 10, imagenUrl: "assets/imagenes/producto1.jpg", cantidad: 2 },
-    { nombre: 'Producto 2', precio: 20, imagenUrl: "assets/imagenes/producto2.jpg", cantidad: 3 },
-    { nombre: 'Producto 3', precio: 30, imagenUrl: "assets/imagenes/producto2.jpg", cantidad: 1 }
+    { nombre: 'Producto 1', precio: 10, imagenUrl: "assets/images/producto1.jpg", cantidad: 2 },
+    { nombre: 'Producto 2', precio: 20, imagenUrl: "assets/images/producto2.jpg", cantidad: 3 },
+    { nombre: 'Producto 3', precio: 30, imagenUrl: "assets/images/producto2.jpg", cantidad: 1 }
   ];
 
   constructor() { }
