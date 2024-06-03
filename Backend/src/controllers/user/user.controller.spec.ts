0076810e23049e0295e78cbcebd8119b70512e4f
AndrewElPresi <andrew.campos.seguel@alumnos.uta.cl>
2024-06-03 02:05:14 -0400

    Update user.controller.spec.ts
    Actualizacion en donde se finalizan las pruebas del user.controller
    1.- Obtener todos los usuarios

--- Backend/src/controllers/user/user.controller.spec.ts
+++ Backend/src/controllers/user/user.controller.spec.ts
@@ -15,6 +15,7 @@ import { UserService } from '../../providers/user/user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -25,7 +26,7 @@ describe('UserController', () => {
         OrderProductService, 
         LocalService,
         {provide: UserService,
-          useValue: {}
+          useValue: {getAll: jest.fn()}
         },
         {provide: ClientService,
           useValue: {}
@@ -49,9 +50,36 @@ describe('UserController', () => {
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService)
   });
   
   it('should be defined!', () => {
         expect(controller).toBeDefined();
     });
+
+  describe('getAllUsers', () =>{
+    it('Should return an Array of Users', async () =>{
+      const response: UserEntity[] = [
+        {
+          userId: 1,
+          username: 'usuario 1',
+          password: 'admin1234'
+        },
+        {
+          userId: 2,
+          username: 'usuario 2',
+          password: 'pass 2'
+        },
+        {
+          userId: 3,
+          username: 'usuario 3',
+          password: 'Subject'
+        }
+      ];
+      jest.spyOn(userService, 'getAll').mockResolvedValue(response)
+      expect(await controller.getAllUsers()).toBe(response)
+      expect(await controller.getAllUsers()).toHaveLength(3)
+    });
+  });
+
 });
