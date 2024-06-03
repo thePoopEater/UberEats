6a3f1bf3f5c5523c1153fd0f3b8a6d3828872891
AndrewElPresi <andrew.campos.seguel@alumnos.uta.cl>
2024-06-03 01:30:31 -0400

    Update local.controller.spec.ts
    Actualizacion del asrchivo spec.ts de pruebas para el local.controller, en donde se terminan todas las pruebas de dicho controlador:
    1.- Obtener todos los locales
    2.- Obtener 1 local en específico
    3.- Modificar la información de 1 local
    4.- Referenciar un local

--- Backend/src/controllers/local/local.controller.spec.ts
+++ Backend/src/controllers/local/local.controller.spec.ts
@@ -4,7 +4,6 @@ import { ProductService } from '../../providers/product/product.service';
 import { OrderService } from '../../providers/order/order.service';
 import { OrderProductService } from '../../providers/order-product/order-product.service';
 import { LocalService } from '../../providers/local/local.service';
-import { Provider } from '@nestjs/common';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { LocalEntity } from '../../database/entities/local.entity';
 import { ProductEntity } from '../../database/entities/product.entity';
@@ -12,6 +11,9 @@ import { OrderEntity } from '../../database/entities/order.entity';
 import { OrderProductEntity } from '../../database/entities/order-products.entity';
 import { ClientService } from '../../providers/client/client.service';
 import { describe, mock } from 'node:test';
+import { LocalCreateDTO } from './dto/local-create.dto';
+import { LocalUpdateDTO } from './dto/local-update.dto';
+import { LocalResponseDTO } from './dto/local-response.dto';
 
 
 describe('LocalController', () => {
@@ -46,4 +48,99 @@ describe('LocalController', () => {
     expect(controller).toBeDefined();
   });
   
+  describe('getAllLocals', () =>{
+    it('Should return an Array of Local', async () =>{
+      const result: Partial<LocalEntity>[] = [
+        { id: 1, 
+          name: 'Local 1',
+          description: 'descripcion 1',
+          address: 'direccion 1',
+          schedule: 'schedule 1',
+          category: 'category 1',
+          orders: [],
+          products: []
+        }, 
+        { id: 2, 
+          name: 'Local 2',
+          description: 'descripcion 2',
+          address: 'direccion 2',
+          schedule: 'schedule 2',
+          category: 'category 2',
+          orders: [],
+          products: []
+        }
+      ];
+      jest.spyOn(localService, 'getAllLocals').mockResolvedValue(result as LocalEntity[]);
+      expect(await controller.getAllLocals()).toBe(result);
+      expect(await controller.getAllLocals()).toHaveLength(2);
+    });
+  });
+
+  describe('getInfo', () =>{
+    it('Should return a Local throught ID', async () =>{
+      const result: Partial<LocalEntity> = 
+        { id: 1, 
+          name: 'Local 1',
+          description: 'descripcion 1',
+          address: 'direccion 1',
+          schedule: 'schedule 1',
+          category: 'category 1',
+          orders: [],
+          products: []
+        };
+      jest.spyOn(localService, 'getLocal').mockResolvedValue(result as LocalEntity);
+      expect(await controller.getInfo(1)).toBe(result)
+    });
+  });
+
+  describe('postLocal', () =>{
+    it('Should return "Se creó un local"', async () =>{
+      const newLocal: LocalCreateDTO = 
+      {
+        name: 'Local 3',
+        description: 'descripcion 3',
+        address: 'direccion 3',
+        schedule: 'schedule 3',
+        category: 'category 3',
+      };
+      const result = {
+        data : "Se creó un local",
+                statusCode: 200,
+                statusDescription : 'Listo',
+                error : null
+      };
+      jest.spyOn(localService, 'create').mockReturnValue(result as any);
+      expect(await controller.postLocal(newLocal)).toStrictEqual(result);
+    });
+  });
+
+  describe('putLocal', () =>{
+    it('Should return an updated local', async () =>{
+      const local: Partial<LocalEntity> = 
+      { id: 3,
+        name: 'Local 3',
+        description: 'descripcion 3',
+        address: 'direccion 3',
+        schedule: 'schedule 3',
+        category: 'category 3',
+      };
+      const update: LocalUpdateDTO = 
+      {
+        name: 'changed Local',
+        address: 'direccion 3',
+        schedule: 'schedule 3',
+        description: 'description 3',
+        category: 'category 3'
+      };
+      const response : LocalResponseDTO = 
+      {
+        data : null,
+        statusCode: 200,
+        statusDescription:'Listo',
+        error : null
+      } as LocalResponseDTO;
+      jest.spyOn(localService, 'updateLocal').mockResolvedValue(response as any)
+      expect(await controller.putLocal(3,update)).toStrictEqual(response)
+    });
+  });
 });
