34503a004118174da936be89dd0d251897f2f794
Daniel <danielalday4@gmail.com>
2024-07-01 10:33:31 -0400

    fix : address service

--- Backend/src/providers/address/address.service.ts
+++ Backend/src/providers/address/address.service.ts
@@ -6,35 +6,49 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class AddressService {
-    constructor(
-        @InjectRepository(AddressEntity) private readonly addressRepository: Repository<AddressEntity>,
-        @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>,
-    ){}
-    //función que permite crear una dirección en el repositorio, primero comprueba que exista 
-    //el id del cliente ingresado para crear la dirección.
-    public async addAddress (data : AddressEntity){
-        const user = await this.userRepository.findOne({
-            where: { userId: data.user?.userId },
-          });  
-          if (!user) {
-            throw new NotFoundException("Cliente no encontrado");
-          }
-          data.user = user; 
-          return await this.addressRepository.save(data);
-        
+  constructor(
+    @InjectRepository(AddressEntity)
+    private readonly addressRepository: Repository<AddressEntity>,
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+  //función que permite crear una dirección en el repositorio, primero comprueba que exista
+  //el id del cliente ingresado para crear la dirección.
+  public async addAddress(data: AddressEntity) {
+    const user = await this.userRepository.findOne({
+      where: { userId: data.user?.userId },
+    });
+    if (!user) {
+      throw new NotFoundException('Cliente no encontrado');
     }
+    data.user = user;
+    return await this.addressRepository.save(data);
+  }
 
-    //Función que busca una dirección específica de la tabla 'address' de acuerdo a un id.
-    public async getAddress(idAddress: number): Promise <AddressEntity>{
-      try{
-          const address = await this.addressRepository.createQueryBuilder('address').where("addressId = :idAddress", { idAddress }).getOne();
-          return address;
-      }catch (error:any){
-          throw new Error(error);
-      }
+  //Función que busca una dirección específica de la tabla 'address' de acuerdo a un id.
+  public async getAddress(idAddress: number): Promise<AddressEntity> {
+    try {
+      const address = await this.addressRepository
+        .createQueryBuilder('address')
+        .where('addressId = :idAddress', { idAddress })
+        .getOne();
+      return address;
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  }
+  public async getAllAdressFromUser(userId: number) {
+    try {
+      const user = await this.userRepository.findOne({
+        where: { userId: userId },
+      });
+      return this.addressRepository.findBy({ user: user });
+    } catch (error: any) {
+      throw new Error(error);
+    }
   }
   //Función que busca todas las direcciones del repositorio de 'address'.
-    public async getAllAddresses() : Promise<AddressEntity[]>{
-      return this.addressRepository.find();
-}
+  public async getAllAddresses(): Promise<AddressEntity[]> {
+    return this.addressRepository.find();
+  }
 }
